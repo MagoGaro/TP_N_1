f8da43d194730cb40bb9f0b5dacf95a276c97935
JULIETA GUTIERREZ <112029156+julyisis@users.noreply.github.com>
2024-04-17 21:29:07 -0300

    Create 4.py

---
+++ ejercicios/4.py
@@ -0,0 +1,8 @@
+#Enunciado
+# 4. Pida al usuario que ingrese 2 números para luego sumarlos y mostrar en pantalla: “La 
+# respuesta es XX
+num1 = input("Ingrese el primer numero: ")
+num2 = input("Ingrese el segundo numero: ")
+
+suma = f"{num1} + {num2}"
+print (f"La respuesta es:  {eval(suma)}")
