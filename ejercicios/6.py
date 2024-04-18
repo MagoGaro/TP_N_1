9a646ea621c846632e20a34214df4a84e40101dc
JULIETA GUTIERREZ <112029156+julyisis@users.noreply.github.com>
2024-04-17 21:41:38 -0300

    Create 6.py

---
+++ ejercicios/6.py
@@ -0,0 +1,11 @@
+#Enunciado
+"""6. Programe una aplicación de consola que pregunte el precio total de la cuenta, luego 
+pregunte cuántos comensales hay. A continuación deberá dividir la cuenta total por el 
+número de comensales y mostrar cuánto debe pagar cada persona."""
+
+#Codigo
+precioTotal = input("Ingrese importe total de su cuenta: ")
+comensales = input("cantidad de personas que comieron: ")
+
+total = f"{precioTotal} / {comensales}"
+print(f"Total por persona : {eval(total)}")
